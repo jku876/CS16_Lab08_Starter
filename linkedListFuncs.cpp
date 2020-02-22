02cb4d88dc4062f12b02ccfe47ca0cf7bc03344c
Johnson Ku <johnsonku@umail.ucsb.edu>
2020-02-22 11:08:16 -0800

    jk -- Changed problem to removeKFromFront

--- linkedListFuncs.cpp
+++ linkedListFuncs.cpp
@@ -1,22 +1,23 @@
 #include "linkedListFuncs.h"
 
-/*Given two linked lists, splice the two linked lists together, alternating elements from each one*/
-/*All methods must be implemented recursively!*/
-Node* merge(Node* head1, Node* head2) {
+/*Given two linked lists, splice the two linked lists together, alternating elements from each one
+ *All methods must be implemented recursively!*/
+Node* splice(Node *head1, Node *head2) {
     return NULL;
     //STUB: edit with the correct output, according to the lab instructions, using recursion
 }
 
-/*Given a head of a linked-list, delete all nodes containing this value*/
-/*All methods must be implemented recursively!*/
-Node* removeTargetValue(Node* head, int target) {
+/*Given the head of a linked list, delete the first k nodes from the linked list
+ *k will always be less than the length of the linked list
+ *All methods must be implemented recursively!*/
+Node* removeKFromFront(Node *head, int k) {
     return NULL;
     //STUB: edit with the correct output, according to the lab instructions, using recursion
 }
 
-/*Given two linked lists, return a linked-list where each element is the sum of the corresponding elements of the input*/
-/*All methods must be implemented recursively!*/
-Node* sum(Node* head1, Node* head2) {
+/*Given two linked lists, return a linked-list where each element is the sum of the corresponding elements of the input
+ *All methods must be implemented recursively!*/
+Node* sum(Node *head1, Node *head2) {
     return NULL;
     //STUB: edit with the correct output, according to the lab instructions, using recursion
 }
