6a1121715a5e5e5c640a5696ac3e512992f7ee57
reznikovl <43658864+reznikovl@users.noreply.github.com>
2020-02-21 17:12:31 -0800

    Update linkedListFuncs.cpp

---
+++ linkedListFuncs.cpp
@@ -0,0 +1,22 @@
+#include "linkedListFuncs.h"
+
+/*Given two linked lists, splice the two linked lists together, alternating elements from each one*/
+/*All methods must be implemented recursively!*/
+Node* merge(Node* head1, Node* head2) {
+    return NULL;
+    //STUB: edit with the correct output, according to the lab instructions, using recursion
+}
+
+/*Given a head of a linked-list, delete all nodes containing this value*/
+/*All methods must be implemented recursively!*/
+Node* removeTargetValue(Node* head, int target) {
+    return NULL;
+    //STUB: edit with the correct output, according to the lab instructions, using recursion
+}
+
+/*Given two linked lists, return a linked-list where each element is the sum of the corresponding elements of the input*/
+/*All methods must be implemented recursively!*/
+Node* sum(Node* head1, Node* head2) {
+    return NULL;
+    //STUB: edit with the correct output, according to the lab instructions, using recursion
+}
