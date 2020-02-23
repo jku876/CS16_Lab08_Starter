f5cbc5c84962c543af176a8df7e7eee57a3b5d85
jku876 <43654791+jku876@users.noreply.github.com>
2020-02-22 23:45:35 -0800

    Good coding practices

---
+++ strFuncs.cpp
@@ -0,0 +1,13 @@
+#include "strFuncs.h"
+#include <cctype>
+
+using namespace std;
+
+
+/* Precondition: s1 is a valid string that may contain upper or lower case alphabets, no spaces or special characters
+ * Postcondition: Returns true if s1 is a palindrome, false otherwise
+ *You should provide a recursive solution*/
+bool isPalindrome(const string s1){
+    //STUB: Replace the following with the correct code.
+    return true;
+}
