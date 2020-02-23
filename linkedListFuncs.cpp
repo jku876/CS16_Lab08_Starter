12857759eeba8a23019664cd230073189a130584
jku876 <43654791+jku876@users.noreply.github.com>
2020-02-22 23:46:54 -0800

    Even better coding practices

---
+++ linkedListFuncs.cpp
@@ -0,0 +1,26 @@
+#include "linkedListFuncs.h"
+#include <stddef.h>
+
+using namespace std;
+
+/*Given two linked lists, splice the two linked lists together, alternating elements from each one
+ *All methods must be implemented recursively!*/
+Node* splice(Node *head1, Node *head2) {
+    return NULL;
+    //STUB: edit with the correct output, according to the lab instructions, using recursion
+}
+
+/*Given the head of a linked list, delete the first k nodes from the linked list
+ *k will always be less than the length of the linked list
+ *All methods must be implemented recursively!*/
+Node* removeKFromFront(Node *head, int k) {
+    return NULL;
+    //STUB: edit with the correct output, according to the lab instructions, using recursion
+}
+
+/*Given two linked lists, return a linked-list where each element is the sum of the corresponding elements of the input
+ *All methods must be implemented recursively!*/
+Node* sum(Node *head1, Node *head2) {
+    return NULL;
+    //STUB: edit with the correct output, according to the lab instructions, using recursion
+}
